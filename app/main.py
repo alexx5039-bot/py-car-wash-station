00d1718393c40c761dda06b658e69915a6d02852
Alex <alexx5039@gmail.com>
2025-10-26 09:36:14 +0200

    CarWashStation implemented

--- app/main.py
+++ app/main.py
@@ -1,8 +1,56 @@
 class Car:
-    # write your code here
-    pass
+
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str
+                 ) -> None:
+        self.comfort_class = comfort_class
+        self.clean_mark = clean_mark
+        self.brand = brand
 
 
 class CarWashStation:
-    # write your code here
-    pass
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_rating: int
+                 ) -> None:
+        self.distance_from_city_center = distance_from_city_center
+        self.clean_power = clean_power
+        self.average_rating = round(average_rating, 1)
+        self.count_of_rating = count_of_rating
+
+    def serve_cars(self, cars: list) -> float:
+        income = 0
+        for car in cars:
+            if car.clean_mark < self.clean_power:
+                income += ((
+                    car.comfort_class
+                    * (self.clean_power - car.clean_mark)
+                    * self.average_rating)
+                    / self.distance_from_city_center
+                )
+                car.clean_mark = self.clean_power
+        return round(income, 1)
+
+    def calculate_washing_price(self, car: "Car") -> float:
+        price = (
+            car.comfort_class
+            * (self.clean_power - car.clean_mark)
+            * self.average_rating
+            / self.distance_from_city_center
+        )
+        return round(price, 1)
+
+    def wash_single_car(self, car: "Car") -> None:
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
+
+    def rate_service(self, rate: float) -> None:
+        total_score = self.average_rating * self.count_of_rating
+        self.count_of_rating += 1
+        self.average_rating = round(
+            (total_score + rate) / self.count_of_rating, 1
+        )
